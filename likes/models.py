1edd0820ac957033715269f929e3b02b2ac431ec
Ahmad-Hazrati <136331424+Ahmad-Hazrati@users.noreply.github.com>
2024-01-30 13:06:42 +0000

    Fixed the bugs in likes model by reseting the database and remigrated

--- likes/models.py
+++ likes/models.py
@@ -20,4 +20,4 @@ class Like(models.Model):
         unique_together = ['owner', 'post']
 
     def __str__(self):
-        return f'{self.owner} {self.post}'
+        f'{self.owner} {self.post}'
--- likes/views.py
+++ likes/views.py
@@ -1,22 +1,27 @@
 from rest_framework import generics, permissions
 from photoapp_api.permissions import IsOwnerOrReadOnly
-from likes.models import Like
-from likes.serializers import LikeSerializer
+from .models import Like
+from .serializers import LikeSerializer
 
 
 class LikeList(generics.ListCreateAPIView):
     """
-    List likes or create a like if logged in.
+    List all likes. Create a like if authenticated.
+    The perform_create method associates the like with the logged in user.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
-
+    
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like. No Update view, as users can only like or unlike
+    a post. Destroy a like, i.e. unlike a post if owner of that like
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
