820959ba383eb99ec418da7ecb27b261a9a6b364
Ahmad-Hazrati <136331424+Ahmad-Hazrati@users.noreply.github.com>
2024-01-31 12:48:07 +0000

    Added extra fields to profiles by annotating queryset

--- profiles/views.py
+++ profiles/views.py
@@ -6,7 +6,8 @@
 # from .serializers import ProfileSerializer
 # from photoapp_api.permissions import IsOwnerOrReadOnly
 
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from photoapp_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -61,7 +62,21 @@ class ProfileList(generics.ListAPIView):
     No create view as profile creation is handled by django signals.
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -70,4 +85,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
