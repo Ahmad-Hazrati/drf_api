3fcbd7541f96dcc3f6c48620658292aade19a29a
Ahmad-Hazrati <136331424+Ahmad-Hazrati@users.noreply.github.com>
2024-02-01 12:00:34 +0000

    Fixed the allowed hostname variable

--- photoapp_api/settings.py
+++ photoapp_api/settings.py
@@ -38,7 +38,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['ALLOWED_HOST', 'localhost']
+ALLOWED_HOSTS = [
+   os.environ.get('ALLOWED_HOST'),
+   'localhost',
+]
 
 
 # Application definition
